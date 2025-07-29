c9370468b907742c7269f2bdd70bb80a6f0fb9a0
Ibicuyno <89782087+Ibicuyno@users.noreply.github.com>
2025-07-29 19:15:54 -0300

    add: primeros archivos para produccion

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// // colocar las credencias propias a cada uno
+// // colocar las credencias propias a cada uno, 1er concepto que nos enseño...
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDwHR_Gcv9QCiHRtFP5mmDnRcPghtMrODQ",
 //   authDomain: "productos-utn.firebaseapp.com",
@@ -14,14 +14,14 @@ import { getAuth } from "firebase/auth";
 //   appId: "1:692029493149:web:ce135f7072d2997be46974"
 // };
 
- // Leo's web app's Firebase configuration
+ // Leo's web app's Firebase configuration AGREGAMOS EL CONCEPTO DE VARIABLES DE ENTORNO. VER ARCHIVO .env
  const firebaseConfig = {
- apiKey: "AIzaSyD-h_JGLKERfKYqfqPctooZdwltRY4E3mA",
- authDomain: "productos-utn-76f8c.firebaseapp.com",
- projectId: "productos-utn-76f8c",
- storageBucket: "productos-utn-76f8c.firebasestorage.app",
- messagingSenderId: "681891639731",
- appId: "1:681891639731:web:124427be68307143eddbc6"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
